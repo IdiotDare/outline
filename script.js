31bc24baafd85d3f97045e2b17d40e98ec8b5f68
IdiotDare <89470696+IdiotDare@users.noreply.github.com>
2021-10-26 10:23:35 -0400

    Update script.js

--- script.js
+++ script.js
@@ -1,3 +1,7 @@
 document.querySelector('button').onclick = function() {
     alert('What does clicking do but waste time? Wait, nvm. Does the table of contents still work?');
 }
+
+document.querySelector('div#sidebar').onclick = function() {
+    alert('how do I do ids');
+}
