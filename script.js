08a1b41b943f90f8aed56c5619716e1f3b161bc5
IdiotDare <89470696+IdiotDare@users.noreply.github.com>
2021-10-26 10:19:20 -0400

    Update script.js

--- script.js
+++ script.js
@@ -1,3 +1,3 @@
-document.querySelector('#button').onclick = function() {
+document.querySelector('button').onclick = function() {
     alert('What does clicking do but waste time? Wait, nvm. Does the table of contents still work?');
 }
