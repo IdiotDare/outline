3810f8d65908b8cd35a6645758b37f144ef7c69a
IdiotDare <89470696+IdiotDare@users.noreply.github.com>
2021-11-01 13:59:04 -0400

    Update script.js

--- script.js
+++ script.js
@@ -52,120 +52,22 @@ function testButton() {//popup random text testing
 
 
 //line break
-
-var saysText = [];
-
-function textGenerato() {//should generator a random string into a var, which is then presented by change_text
-    var num = Math.floor(Math.random()*(10-1)+1);
-    switch(num+0){
-        case 1:
-            saysText.push("I know right?");
-            break;
-        case 2:
-            saysText.push("Confusing, huh?");
-            break;
-        case 3:
-            saysText.push("I don't know what you want.");
-            break;
-        case 4:
-            saysText.push("I'm not sure why you're asking me.");
-            break;
-        case 5:
-            saysText.push("I don't think you should ask me.");
-            break;
-        case 6:
-            saysText.push("I...");
-            break;
-        case 7:
-            saysText.push("I...am not exactly unsure.");
-            break;
-        case 8:
-            saysText.push("I don't get it...")
-            break;
-        case 9:
-            saysText.push("Sorry. I don't understand.")
-            break;
-        case 10:
-            saysText.push("I don't know. Do I look like I even care? ...Sorry.");
-            break;
-        default:
-            saysText.push("Please stop.");
-            break;
     }
 }
 
-
-function change_text(){//button onclick calls this function
-    textGenerato()
-    document.getElementById("text").innerHTML = saysText[0];
-    
-}
-
-//    document.getElementById("text").innerHTML = "You clicked the button, I am new paragraph.";
+// onclick="change_text" button id is #randotext paragraph id is "text" no hastag needed to mention ids
+/* Text options:
 
 
-
-
-
-
-
-
-
-/*
-document.querySelector('#randotext').onclick = function() {
-    var num = Math.floor(Math.random()*(10-1)+1);
-    switch(num+0){
-    case 1:
-        alert('I know right?);
-        break;
-    case 2:
-        alert('Confusing, huh?');
-        break;
-    case 3:
-        alert("I don't know what you want.");
-        break;
-    case 4:
-        alert("I'm not sure why you're asking me.")
-        break;
-    case 5:
-        alert("I don't think you should ask me.")
-        break;
-    case 6:
-        alert("I...")
-        break;
-        
-    default:
-        alert("Please stop.")      
- }    
-}
-*/
-/*
-function randotext() {
-    var num = Math.floor(Math.random()*(10-1)+1);
-    switch(num){
-    case num == 1:
-        alert('I know right?);
-        break;
-    case num == 2:
-        alert('Confusing, huh?');
-        break;
-    case num == 3:
-        alert("I don't know what you want.");
-        break;
-    case num == 4:
-        alert("I'm not sure why you're asking me.")
-        break;
-    case num == 5:
-        alert("I don't think you should ask me.")
-        break;
-    case num == 6:
-        alert("I...")
-        break;
-        
-    default:
-        alert("Please stop.")      
- }    
-}
-
-document.querySelector('#randotext').onclick = randotext()
-*/
+            "I know right?"
+            "Confusing, huh?"
+            "I don't know what you want."
+            "I'm not sure why you're asking me."
+            "I don't think you should ask me."
+            "I..."
+            "I...am not exactly unsure."
+            "I don't get it..."
+            "Sorry. I don't understand."
+            "I don't know. Do I look like I even care? ...Sorry."
+            "Please stop.";
+            */
