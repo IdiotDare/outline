fa2c37b6bbad82a5608a8317256b365c32cedf48
IdiotDare <89470696+IdiotDare@users.noreply.github.com>
2021-11-01 10:20:44 -0400

    Update script.js

--- script.js
+++ script.js
@@ -9,6 +9,48 @@ document.querySelector('#sidebar').onclick = function() {
 document.querySelector('#update').onclick = function() {
     alert('Before you ask, yes, I have absolutely no idea what I\'m doing. Seriously. :P I\m still learning basic JS.');
 }
+
+
+
+function change_text(){
+    
+    var num = Math.floor(Math.random()*(10-1)+1);
+    return num
+    switch(num){
+    case num == 1:
+        document.getElementById("text").innerHTML = 'I know right?';
+        break;
+    case num == 2:
+        document.getElementById("text").innerHTML = 'Confusing, huh?';
+        break;
+    case num == 3:
+        document.getElementById("text").innerHTML = "I don't know what you want.";
+        break;
+    case num == 4:
+        document.getElementById("text").innerHTML = "I'm not sure why you're asking me.";
+        break;
+    case num == 5:
+        document.getElementById("text").innerHTML = "I don't think you should ask me.";
+        break;
+    case num == 6:
+        document.getElementById("text").innerHTML = "I...";
+        break;
+        
+    default:
+        alert("Please stop.")  document.getElementById("text").innerHTML = "Please stop.";
+   }
+}
+
+//    document.getElementById("text").innerHTML = "You clicked the button, I am new paragraph.";
+
+
+
+
+
+
+
+
+
 /*
 document.querySelector('#randotext').onclick = function() {
     var num = Math.floor(Math.random()*(10-1)+1);
