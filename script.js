ec4404143f772faa28f1df85b7f4ddaafde5adb1
IdiotDare <89470696+IdiotDare@users.noreply.github.com>
2021-11-01 09:00:05 -0400

    Update script.js

--- script.js
+++ script.js
@@ -10,7 +10,7 @@ document.querySelector('#update').onclick = function() {
     alert('Before you ask, yes, I have absolutely no idea what I\'m doing. Seriously. :P I\m still learning basic JS.');
 }
 
-document.querySelector('#randotext').onclick = function() {
+/* document.querySelector('#randotext').onclick = function() {
     var num = Math.floor(Math.random()*(10-1)+1);
     switch(num){
     case num == 1:
@@ -36,3 +36,32 @@ document.querySelector('#randotext').onclick = function() {
         alert("Please stop.")      
  }    
 }
+*/
+function randotext() {
+    var num = Math.floor(Math.random()*(10-1)+1);
+    switch(num){
+    case num == 1:
+        alert('I know right?);
+        break;
+    case num == 2:
+        alert('Confusing, huh?');
+        break;
+    case num == 3:
+        alert("I don't know what you want.");
+        break;
+    case num == 4:
+        alert("I'm not sure why you're asking me.")
+        break;
+    case num == 5:
+        alert("I don't think you should ask me.")
+        break;
+    case num == 6:
+        alert("I...")
+        break;
+        
+    default:
+        alert("Please stop.")      
+ }    
+}
+
+document.querySelector('#randotext').onclick = randotext()
